2281f47368ae0242d1a3f58fa3af4df2851373fc
Sideral Geek Oficial <105156835+SideralGeek@users.noreply.github.com>
2022-12-01 09:14:51 -0300

    Banner
    Atualização JS

--- visao/js/scritp-banner.js
+++ visao/js/scritp-banner.js
@@ -1,18 +1,18 @@
 /* Formulário "Cor Sólida" ou "Imagem" */
 
 //Esconde ambos forumários
-document.getElementById('formCorUpload').style.display='none';
-document.getElementById('formCorSelect').style.display='none';
+document.getElementById('CorImagem').style.display='none';
+document.getElementById('CorSolida').style.display='none';
 
 //Recebe qual valor que está no input radio
 function hideShowDiv(val){
     //Relativamente esconde um form e mostrando outro inversamente
     if(val==1){
-        document.getElementById('formCorUpload').style.display='none';
-        document.getElementById('formCorSelect').style.display='block';
+        document.getElementById('CorSolida').style.display='block';
+        document.getElementById('CorImagem').style.display='none';
     }
     if(val==2){
-        document.getElementById('formCorSelect').style.display='none';
-        document.getElementById('formCorUpload').style.display='block';
+        document.getElementById('CorSolida').style.display='none';
+        document.getElementById('CorImagem').style.display='block';
     }
 }
